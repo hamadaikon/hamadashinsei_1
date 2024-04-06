b191f16811b32e59048286e20aa664b4f4375b4f
hamadaikon <156398571+hamadaikon@users.noreply.github.com>
2024-04-07 04:44:10 +0900

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('a[href^=#]').click(function() {
+        var speed = 70; // スクロール速度(ミリ秒)
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $('html').animate({scrollTop:position}, speed,'swing');return false;
+    });
+});
